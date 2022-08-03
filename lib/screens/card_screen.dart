68779c819a8de2ba842546563e77c67849138bf7
gemydev <mohamed_gamal1146@yahoo.com>
2022-08-03 04:03:37 +0200

    add payment with card

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../utils/constants.dart';
 
 class CardScreen extends StatefulWidget {
   const CardScreen({Key? key}) : super(key: key);
@@ -10,13 +14,47 @@ class CardScreen extends StatefulWidget {
 }
 
 class _CardScreenState extends State<CardScreen> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: WebView(
-          initialUrl: "",
-
-        )
+    return Scaffold(
+      body: WebView(
+        initialUrl:
+            "https://accept.paymob.com/api/acceptance/iframes/439958?payment_token=$finalTokenCard",
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        onProgress: (int progress) {
+          debugPrint('WebView is loading (progress : $progress%)');
+        },
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            debugPrint('blocking navigation to $request}');
+            return NavigationDecision.prevent;
+          }
+          debugPrint('allowing navigation to $request');
+          return NavigationDecision.navigate;
+        },
+        onPageStarted: (String url) {
+          debugPrint('Page started loading: $url');
+        },
+        onPageFinished: (String url) {
+          debugPrint('Page finished loading: $url');
+        },
+        gestureNavigationEnabled: true,
+        backgroundColor: const Color(0x00000000),
+      ),
     );
   }
 }
